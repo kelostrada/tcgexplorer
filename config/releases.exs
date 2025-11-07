bd8f20e2e55113f74b1f4fa04ccaf9904e5ca4b4
Bartosz Kalinowski <kelostrada@gmail.com>
2025-11-07 20:52:17 +0100

    fix https connection

--- config/releases.exs
+++ config/releases.exs
@@ -28,9 +28,10 @@ host =
 
 config :tcg_explorer, TcgExplorerWeb.Endpoint,
   server: true,
-  url: [host: host],
+  url: [host: host, scheme: "https", port: 443],
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
+  check_origin: ["https://#{host}"],
   secret_key_base: secret_key_base
